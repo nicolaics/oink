1ce37921afac12d172285c17bd4904c28c63ca4d
nicolaics <nicolai.christian.s@gmail.com>
2024-08-24 17:05:13 +0900

    add account number, active bool for loan

--- BACKEND/db/angelhack hackathon 2024.sql
+++ BACKEND/db/angelhack hackathon 2024.sql
@@ -21,9 +21,11 @@ CREATE TABLE `loan` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
   `debtor_id` int NOT NULL,
   `amount` float NOT NULL,
+  `amount_paid` float NOT NULL DEFAULT 0.0,
   `start_date` timestamp NOT NULL DEFAULT (now()),
   `end_date` timestamp NOT NULL,
   `duration` varchar(255) NOT NULL,
+  `active` boolean DEFAULT FALSE,
 
   FOREIGN KEY (`debtor_id`) REFERENCES `users` (`id`)
 );
@@ -31,6 +33,7 @@ CREATE TABLE `loan` (
 CREATE TABLE `account` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
   `user_id` int NOT NULL,
+  `number` varchar (512) UNIQUE NOT NULL,
   `balance` float NOT NULL DEFAULT 0,
 
   FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
@@ -39,6 +42,7 @@ CREATE TABLE `account` (
 CREATE TABLE `savings_account` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
   `user_id` int NOT NULL,
+  `number` varchar (512) UNIQUE NOT NULL,
   `amount` float NOT NULL DEFAULT 0,
 
   FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
