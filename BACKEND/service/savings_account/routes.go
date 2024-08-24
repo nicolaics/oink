f55246a460dab7420cf1d65d47e5f9e346843efb
nicolaics <nicolai.christian.s@gmail.com>
2024-08-24 14:11:43 +0900

    add get savings amount api

--- BACKEND/service/savings_account/routes.go
+++ BACKEND/service/savings_account/routes.go
@@ -20,12 +20,12 @@ func NewHandler(store types.SavingsAccountStore) *Handler {
 
 func (h *Handler) RegisterRoutes(router *mux.Router) {
 	router.HandleFunc("/savings-account/savings", h.handleUpdateSavingsAmount).Methods(http.MethodPatch)
-	// router.HandleFunc("/user/register", h.handleRegister).Methods("POST")
+	router.HandleFunc("/savings-account/savings-amount", h.handleGetSavingsAmount).Methods(http.MethodGet)
 }
 
 func (h *Handler) handleUpdateSavingsAmount(w http.ResponseWriter, r *http.Request) {
 	// get JSON Payload
-	var payload types.UpdateSavingsAmountPayload
+	var payload types.SavingsAmountPayload
 
 	if err := utils.ParseJSON(r, &payload); err != nil {
 		utils.WriteError(w, http.StatusBadRequest, err)
@@ -52,4 +52,28 @@ func (h *Handler) handleUpdateSavingsAmount(w http.ResponseWriter, r *http.Reque
 	}
 
 	utils.WriteJSON(w, http.StatusOK, nil)
+}
+
+func (h *Handler) handleGetSavingsAmount(w http.ResponseWriter, r *http.Request) {
+	// get JSON Payload
+	var payload types.SavingsAmountPayload
+
+	if err := utils.ParseJSON(r, &payload); err != nil {
+		utils.WriteError(w, http.StatusBadRequest, err)
+	}
+
+	// validate the payload
+	if err := utils.Validate.Struct(payload); err != nil {
+		errors := err.(validator.ValidationErrors)
+		utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("invalid payload: %v", errors))
+		return
+	}
+
+	savingsAcc, err := h.store.GetSavingsAccountByID(payload.UserID)
+	if err != nil {
+		utils.WriteError(w, http.StatusBadRequest, fmt.Errorf("not found, invalid account ID"))
+		return
+	}
+
+	utils.WriteJSON(w, http.StatusOK, savingsAcc)
 }
