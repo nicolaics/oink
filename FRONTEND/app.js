269523714c27f08a14e1e6579e2e3ed56004b14e
leegoreu <igor.bruhov2002@gmail.com>
2024-08-25 02:10:26 +0900

    biiiiig

--- FRONTEND/app.js
+++ FRONTEND/app.js
@@ -3,6 +3,11 @@
 document.addEventListener("DOMContentLoaded", function() {
     // Construct the API URL using the environment variable
     const apiUrl = `http://${BACKEND_ROOT}/api/v1/account/balance`;
+    var imgElement = document.getElementById("clickableGif");
+    var savings = 0;
+    var userId = 0;
+    userId = localStorage.getItem("userId");
+    console.log(userId);
     
     fetch(apiUrl, {
         method: "POST",
@@ -10,7 +15,7 @@ document.addEventListener("DOMContentLoaded", function() {
             "Content-Type": "application/json"
         },
         body: JSON.stringify({
-            userId: 1
+            userId: parseInt(userId)
         })
     }).then(response => response.json())
         .then(data => {
@@ -30,12 +35,17 @@ document.addEventListener("DOMContentLoaded", function() {
             "Content-Type": "application/json"
         },
         body: JSON.stringify({
-            userId: 1
+            userId: parseInt(userId)
         })
     }).then(response2 => response2.json())
         .then(data2 => {
             // Assuming the data structure is as provided
             document.getElementById('savingsAmountBroken').textContent = `₩${data2.amount.toFixed(2)} collected!`;
+            savings = data2.amount.toFixed(2);
+            if (savings < 1){
+                imgElement.src = "img/brokenPiggyBankStatic.png";
+                imgElement.title = "Already Broken!"
+            }
         })
         .catch(error2 => {
             console.error('Error fetching account data:', error2);
@@ -44,14 +54,14 @@ document.addEventListener("DOMContentLoaded", function() {
 
 document.addEventListener("DOMContentLoaded", function() {
     const apiUrl = `http://${BACKEND_ROOT}/api/v1/savings-account`;
-
+    
     fetch(apiUrl, {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
         },
         body: JSON.stringify({
-            userId: 1
+            userId: parseInt(userId)
         })
     }).then(response => response.json())
         .then(data => {
@@ -71,6 +81,27 @@ document.addEventListener("DOMContentLoaded", function() {
 function playGifOnce() {
     var imgElement = document.getElementById("clickableGif");
     var currentSrc = imgElement.src.split('/').pop();
+    var userId = 0;
+    userId = localStorage.getItem("userId");
+    const apiUrl2 = `http://${BACKEND_ROOT}/api/v1/savings-account`;
+    var savings = 0;
+
+    fetch(apiUrl2, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            userId: parseInt(userId)
+        })
+    }).then(response2 => response2.json())
+        .then(data2 => {
+            // Assuming the data structure is as provided
+            savings = data2.amount.toFixed(2);
+        })
+        .catch(error2 => {
+            console.error('Error fetching account data:', error2);
+        });
 
     if (currentSrc === "piggyBankStatic.png") {
         imgElement.src = "img/piggyBank.gif";
@@ -90,6 +121,8 @@ function showPopup() {
 function closePopup() {
     document.getElementById("overlay").style.display = "none";
     document.getElementById("popup").style.display = "none";   
+    var userId = 0;
+    userId = localStorage.getItem("userId");
 
     const apiUrl2 = `http://${BACKEND_ROOT}/api/v1/savings-account`;
 
@@ -99,7 +132,7 @@ function closePopup() {
             "Content-Type": "application/json"
         },
         body: JSON.stringify({
-            userId: 1
+            userId: parseInt(userId)
         })
     });
 }
