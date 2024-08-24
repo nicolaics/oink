71a52a7cce4fd5e5c9cea4873c3e1fe270d0a799
nicolaics <nicolai.christian.s@gmail.com>
2024-08-24 17:05:03 +0900

    add account number, active bool for loan

--- BACKEND/cmd/migrate/migrations/20240824045142_add-savings-account-table.up.sql
+++ BACKEND/cmd/migrate/migrations/20240824045142_add-savings-account-table.up.sql
@@ -1,6 +1,7 @@
 CREATE TABLE `savings_account` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
   `user_id` int NOT NULL,
+  `number` varchar (512) UNIQUE NOT NULL,
   `amount` float NOT NULL DEFAULT 0,
 
   FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
